e718ea5af4520036c4aaabd85021fd6805a6f0fd
Meg-brennan <megcbren@gmail.com>
2021-10-17 16:12:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather />
-      </header>
+      <Weather />
+      <Footer />
     </div>
   );
 }
