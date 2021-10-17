26550919890872191c233b66c81445a61b0b2ea6
Meg-brennan <megcbren@gmail.com>
2021-10-17 16:08:34 -0700

    Add footer js and css

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function () {
+  return (
+    <div className="Footer">
+      <a href="/">Open-source code</a> by Megan Brennan
+    </div>
+  );
+}
