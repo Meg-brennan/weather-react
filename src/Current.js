b9d1f2e031ae89d572c70175c2ccb78c1ffc0004
Meg-brennan <megcbren@gmail.com>
2021-10-17 17:31:00 -0700

    Update Current.js, Current.css, and include Current in Weather.js

---
+++ src/Current.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Current.css";
+
+export default function Current(props) {
+  return (
+    <div className="Current">
+      <div className="row display-flex">
+        <div className="col-4 align-self-center">
+          <div className="text-center">
+            <div className="description">{props.description}</div>
+            <img src={props.imgUrl} alt={props.description} />
+          </div>
+        </div>
+        <div className="col-4 align-self-center text-center">
+          <div className="current-temperature">
+            <div>
+              <strong>{props.temperature}</strong>
+              <span className="units">
+                <a href="/" className="active">
+                  F
+                </a>{" "}
+                <a href="/">C</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 align-self-center">
+          <ul>
+            <li>Humidity: {props.humidity}%</li>
+            <li>Wind: {props.wind} mi/hr</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Weather.css";
 import Search from "./Search";
+import Current from "./Current";
 
 export default function Weather() {
   let weatherData = {
@@ -17,6 +18,13 @@ export default function Weather() {
       <Search />
       <h1>{weatherData.city}</h1>
       <h2>Last updated: {weatherData.date}</h2>
+      <Current
+        description={weatherData.description}
+        imgUrl={weatherData.imgUrl}
+        temperature={weatherData.temperature}
+        humidity={weatherData.humidity}
+        wind={weatherData.wind}
+      />
     </div>
   );
 }
