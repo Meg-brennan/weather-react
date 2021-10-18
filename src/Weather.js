7c46836dead1dad404bab013652475511b464def
Meg-brennan <megcbren@gmail.com>
2021-10-17 17:21:41 -0700

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,5 +3,20 @@ import "./Weather.css";
 import Search from "./Search";
 
 export default function Weather() {
-  return <Search />;
+  let weatherData = {
+    city: "Tokyo",
+    date: "Friday 6:00pm",
+    description: "Partly Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    temperature: 19,
+    humidity: 80,
+    wind: 10,
+  };
+  return (
+    <div className="Weather">
+      <Search />
+      <h1>{weatherData.city}</h1>
+      <h2>Last updated: {weatherData.date}</h2>
+    </div>
+  );
 }
