904b050646a2a3bcb2435eac163080533960ca6b
Meg-brennan <megcbren@gmail.com>
2021-10-17 19:25:24 -0700

    Update Footer.js function name and link security

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Footer.css";
 
-export default function () {
+export default function Footer() {
   return (
     <div className="Footer">
-      <a href="https://github.com/Meg-brennan/weather-react" target="_blank">
+      <a
+        href="https://github.com/Meg-brennan/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>{" "}
       by Megan Brennan
